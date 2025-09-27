a4bdec7ab41e09f5f14d6d3fe0e8cf9446a070eb
erdemkrgn <muammererdemkaragun@hotmail.com>
2025-09-27 16:36:32 +0300

    shadowing issue

--- MuammerErdemKaragun.js
+++ MuammerErdemKaragun.js
@@ -1,6 +1,6 @@
 //page
 const getRequestUrl = "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json";
-const localStorage = {
+const locStorage = {
     Products: "ebebek_products_cache",
     CachedDate: "ebebek_products_cached_date",
     Favs: "ebebek_favorites"
@@ -11,19 +11,19 @@ if (!isHome) { console.log("Wrong Page"); return; }
 
 //fonctions,calculations
 
-const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+const save = (key, value) => locStorage.setItem(key, JSON.stringify(value));
 const read = (key, failValue = null) => {
     try {
-        const value = localStorage.getItem(key);
+        const value = locStorage.getItem(key);
         return value ? parse(value) : failValue;
     } catch {
         return failValue;
     }
 };
 const turkishLiraFormatter = number => new Intl.NumberFormat("tr-TR", { style: "currency", currency: "TRY", maximumFractionDigits: 2 }).format(number);
-const discountPercentage = (o_price, c_price) => (o_price > 0 && c_price < o_price) ? Math.round((1 - c / o) * 100) : 0;
-const favProdSet = () => new Set((read(localStorage.Favs, []) || []).map(id => String(id)));
-const writeFavProdSet = products => save(localStorage.Favs, Array.from(products));
+const discountPercentage = (o_price, c_price) => (o_price > 0 && c_price < o_price) ? Math.round((1 - c_price / o_price) * 100) : 0;
+const favProdSet = () => new Set((read(locStorage.Favs, []) || []).map(id => String(id)));
+const writeFavProdSet = products => save(locStorage.Favs, Array.from(products));
 
 const product = p => ({ // I assume that the attributes of the object coming from the GET request are not null. Thats why i dont add default values.
     id: String(p.id),
@@ -40,13 +40,13 @@ const fetchProducts = async () => {
     if (!response.ok) throw new Error('Fetch failed ${response.status}');
     const raw = await response.json();
     const items = raw.map(product);
-    save(localStorage.Products, items);
-    save(localStorage.CachedDate, Date.now());
+    save(locStorage.Products, items);
+    save(locStorage.CachedDate, Date.now());
     return items;
 }
 // if local storage contains cached items, get them
 const getProductsFromLocale = async () => {
-    const cached = read(localStorage.Products);
+    const cached = read(locStorage.Products);
     if (Array.isArray(cached) && cached.length) return cached;
     return await fetchProducts();
 }
